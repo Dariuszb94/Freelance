2d7595791fa23797246c3ce38775a5c98b307795
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-06-10 21:19:09 +0200

    fixed

---
+++ src/components/Video/Video.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { VideoScroll } from "react-video-scroll";
+import VideoSrc from "../../assets/video.mp4";
+export const Video = () => {
+  const onScroll = (props) => {
+    console.log(props);
+  };
+  return (
+    <React.Fragment>
+      <VideoScroll
+        playbackRate={254}
+        style={{ position: "fixed" }}
+        onScroll={onScroll}
+      >
+        <video
+          tabIndex="0"
+          autobuffer="autobuffer"
+          preload="preload"
+          style={{ width: "100%", objectFit: "contain" }}
+          playsInline
+        >
+          <source type="video/mp4" src={VideoSrc} />
+        </video>
+      </VideoScroll>
+    </React.Fragment>
+  );
+};
+
+export default Video;
