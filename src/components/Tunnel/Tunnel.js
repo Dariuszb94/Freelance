75e261dd635c82d7ecb029e6a9404200dd1b40da
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-06-12 14:24:01 +0200

    second box

--- src/components/Tunnel/Tunnel.js
+++ src/components/Tunnel/Tunnel.js
@@ -1,27 +1,39 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
 import "./Tunnel.css";
-import { useSpring, animated, interpolate } from "react-spring";
-import Aos from "aos";
-import { useInView } from "react-intersection-observer";
+import { useSpring, animated as a, interpolate } from "react-spring";
 import "aos/dist/aos.css";
 const Tunnel = () => {
-  const tunnel__inner = useRef(null);
   const tunnel__outer = useRef(null);
 
   const [offset, offsetSet] = useState(0);
-
-  const { ref, inView, entry } = useInView({
-    threshold: 0,
+  const contentPropsLT = useSpring({
+    opacity: offset,
+    left: `${50 - 50 * offset}%`,
+    top: `${50 - 50 * offset}%`,
+    transform: `translate(${-50 + 50 * offset}%, ${
+      -50 + 50 * offset
+    }%) rotate(${offset * 90}deg)`,
   });
-
+  const contentPropsRT = useSpring({
+    opacity: offset,
+    left: `${50 + 50 * offset}%`,
+    top: `${50 - 50 * offset}%`,
+    transform: `translate(${-100 * offset}%, ${-50 + 50 * offset}%) rotate(${
+      offset * -90
+    }deg)`,
+  });
   useEffect(() => {
     const onScroll = (e) => {
       const offsetRaw =
         (e.target.documentElement.scrollTop -
           tunnel__outer.current.offsetTop +
           window.innerHeight) /
-        (tunnel__outer.current.clientHeight + window.innerHeight);
-      offsetRaw > 0 && offsetRaw < 1 ? offsetSet(offsetRaw) : offsetSet(0);
+        (tunnel__outer.current.clientHeight - 100);
+      if (offsetRaw < 0) offsetSet(0);
+
+      if (offsetRaw > 0 && offsetRaw <= 1) offsetSet(offsetRaw);
+
+      if (offsetRaw > 1) offsetSet(1);
     };
     window.addEventListener("scroll", onScroll);
 
@@ -29,11 +41,14 @@ const Tunnel = () => {
   }, [offset]);
 
   return (
-    <animated.section className="tunnel" ref={tunnel__outer}>
-      <div ref={tunnel__inner} className="tunnel__sticky">
-        <div ref={ref}>aa</div>
+    <a.section className="tunnel" ref={tunnel__outer}>
+      <div className="tunnel__sticky">
+        <a.div style={contentPropsLT} className="tunnel__box__lt"></a.div>
+        <a.div style={contentPropsRT} className="tunnel__box__rt"></a.div>
+        {/* <a.div style={contentPropsLB} className="tunnel__box__lb"></a.div>
+        <a.div style={contentPropsRB} className="tunnel__box__rb"></a.div> */}
       </div>
-    </animated.section>
+    </a.section>
   );
 };
 
