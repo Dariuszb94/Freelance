a9dea6c38896c5fd60f689c36e722626d3db0850
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-06-20 17:13:09 +0200

    cards 1.0

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,3 +1,4 @@
+import { render } from "react-dom";
 import React, { useState } from "react";
 import { useSprings, animated, interpolate } from "react-spring";
 import { useGesture } from "react-use-gesture";
@@ -12,22 +13,21 @@ const cards = [
   "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg",
 ];
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = (i) => ({
-  x: 0,
-  y: i * -4,
-  scale: 1,
-  rot: -10 + Math.random() * 20,
-  delay: i * 100,
-});
-const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r, s) =>
-  `perspective(1500px) rotateX(30deg) rotateY(${
-    r / 10
-  }deg) rotateZ(${r}deg) scale(${s})`;
-
 export default function Deck() {
+  // These two are just helpers, they curate spring data, values that are later being interpolated into css
+  const to = (i) => ({
+    x: 0,
+    y: i * -4,
+    scale: 1,
+    rot: -10 + Math.random() * 20,
+    delay: i * 100,
+  });
+  const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+  // This is being used down there in the view, it interpolates rotation and scale into a css transform
+  const trans = (r, s) =>
+    `perspective(1500px) rotateX(30deg) rotateY(${
+      r / 10
+    }deg) rotateZ(${r}deg) scale(${s})`;
   const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
   const [props, set] = useSprings(cards.length, (i) => ({
     ...to(i),
@@ -43,7 +43,6 @@ export default function Deck() {
       direction: [xDir],
       velocity,
     }) => {
-      console.log("ASdas");
       const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
       const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
       if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
@@ -66,24 +65,34 @@ export default function Deck() {
     }
   );
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-  return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div
-      key={i}
-      style={{
-        transform: interpolate(
-          [x, y],
-          (x, y) => `translate3d(${x}px,${y}px,0)`
-        ),
-      }}
-    >
-      {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-      <animated.div
-        {...bind(i)}
-        style={{
-          transform: interpolate([rot, scale], trans),
-          backgroundImage: `url(${cards[i]})`,
-        }}
-      />
-    </animated.div>
-  ));
+  return (
+    <section className="projects">
+      <div className="projects__inner">
+        {props.map(({ x, y, rot, scale }, i) => (
+          <animated.div
+            className="card-container"
+            key={i}
+            style={{
+              transform: interpolate(
+                [x, y],
+                (x, y) => `translate3d(${x}px,${y}px,0)`
+              ),
+            }}
+          >
+            {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+            <animated.div
+              {...bind(i)}
+              style={{
+                transform: interpolate([rot, scale], trans),
+                backgroundImage: `url(${cards[i]})`,
+              }}
+              className="card"
+            />
+          </animated.div>
+        ))}
+      </div>
+    </section>
+  );
 }
+
+render(<Deck />, document.getElementById("root"));
