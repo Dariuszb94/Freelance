1b977ba08863f0ce86fecb8c977025bf900519c5
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-06-23 22:06:50 +0200

    swipe

--- src/components/Cards/CardsComponent/Bg.js
+++ src/components/Cards/CardsComponent/Bg.js
@@ -1,6 +1,40 @@
 import React from "react";
+import {
+  useSprings,
+  animated as a,
+  interpolate,
+  useSpring,
+} from "react-spring";
 
 function Bg() {
+  const swipeRight = useSpring({
+    from: {
+      transform: "translateX(-50px)",
+    },
+    to: async (next) => {
+      while (1) {
+        await next({
+          transform: "translateX(10px)",
+        });
+
+        await next({ transform: "translateX(60px)" });
+      }
+    },
+  });
+  const swipeLeft = useSpring({
+    from: {
+      transform: "translateX(50px)",
+    },
+    to: async (next) => {
+      while (1) {
+        await next({
+          transform: "translateX(-10px)",
+        });
+
+        await next({ transform: "translateX(-60px)" });
+      }
+    },
+  });
   return (
     <svg
       width="355"
@@ -10,20 +44,17 @@ function Bg() {
       className="projects__bg"
       fill="#8306b1"
     >
-      <path
+      <a.path
+        style={swipeRight}
         d="M281.5 166.57L352.357 236L281.5 305.43V279V277.5H280H1.5V194.5H280H281.5V193V166.57Z"
         stroke="black"
         stroke-width="3"
       />
-      <path
+      <a.path
         d="M73 142.43L2.1432 73L73 3.56987V30V31.5H74.5L353 31.5V114.5L74.5 114.5H73V116V142.43Z"
         stroke="black"
         stroke-width="3"
-      />
-      <path
-        d="M281.5 166.57L352.357 236L281.5 305.43V279V277.5H280H1.5V194.5H280H281.5V193V166.57Z"
-        stroke="black"
-        stroke-width="3"
+        style={swipeLeft}
       />
     </svg>
   );
