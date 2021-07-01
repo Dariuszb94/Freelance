f4f8ba1f074d7088c2358aefa5312cd296df938a
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-07-01 18:27:26 +0200

    menu show

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import "./Menu.scss";
 const Menu = () => {
+  const [menuShow, menuShowSet] = useState(false);
+
   return (
     <section>
-      <MenuIcon className="menu-icon" />
+      <MenuIcon className="menu-icon" onClick={menuShowSet()} />
     </section>
   );
 };
